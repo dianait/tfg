b04209dcd9db31c7921f459b7a06edada2082f6c
Diana Hernández Soler <dianahdezsoler@gmail.com>
2022-07-02 09:57:00 +0200

    Add test

---
+++ src/test/utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+PKG = 'action_template'
+import sys  
+import unittest
+import rospy
+sys.path.append('/home/diana/catkin_ws/src/action-template/src')     
+from Utils import Utils
+
+class MyTestCase1(unittest.TestCase):
+
+    def setUp(self):
+        self.utils = Utils()
+        self.userdata = {'name': 'Jinko', 'questions': 2, 'difficulty': 'facil'}
+
+    def test_data_correct(self):
+        outcome = self.utils.isInfoCorrect( self.userdata )
+        self.assertEquals(outcome, True)
+
+    def test_save_data_correct(self):
+        self.utils.saveData( self.userdata)
+        outcome = self.utils.getParams()
+        self.assertEquals(outcome, self.userdata)
+
+if __name__ == '__main__':
+    import rosunit
+    rospy.init_node("test")
+    rosunit.unitrun(PKG, 'MyTestCase1', MyTestCase1)
