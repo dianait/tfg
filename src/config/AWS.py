b638511b79292983511e73a0ebc272a45e2adf78
Diana Hernández <dianahdezsoler@gmail.com>
2022-07-11 22:39:07 +0200

    Delete AWS.py

--- src/config/AWS.py
+++
@@ -1,6 +0,0 @@
-AWSsesion = [
-    "AKIA6HVWCAWGD4AJ3ZXR", # access key id 
-    "iJtnPtQ5W530ocrk99bOsBSGXvhobTMqmHHVJf1Y", # secret access key
-    "", # token, dejalo vacio
-    "us-east-1" # region
-]
